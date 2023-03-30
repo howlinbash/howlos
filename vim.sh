01a3a1e53f7c2cca2b7113cb5cac04aa073b2663
Howlin Bash <howlinbash@posteo.de>
2023-03-30 14:28:37 +0300

    Fix mv errors

--- vim.sh
+++ vim.sh
@@ -16,10 +16,12 @@ mkdir -p src builds corkboard gits sandbox tmp Pictures/screenshots
 cd src
 git clone https://github.com/howlinbash/dotfiles
 shopt -s dotglob
+mv -f dotfiles/.config/gtk-3.0/* ~/.config/gtk-3.0/
+rm -rf dotfiles/.config/gtk-3.0
 mv dotfiles/.config/* ~/.config/
 rm -rf dotfiles/.config
-mv dotfiles/bin ~/.local/
-rm -rf dotfiles/bin
+mv dotfiles/.local/* ~/.local/
+rm -rf dotfiles/.local
 mv dotfiles/* ~/
 rm -rf dotfiles
 cd ~
